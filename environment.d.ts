5e89cde6e3ae648e37605c9a958f849c08b917ed
Borja <borja@Ordenador-portatil-de-Borja.local>
2024-04-17 00:43:06 +0200

    .

--- environment.d.ts
+++ environment.d.ts
@@ -1,9 +1,13 @@
 namespace NodeJS {
-    interface ProcessEnv {
-        STRIPE_SECRET_KEY: string;
-        BASE_URL: string;
-        NEXT_PUBLIC_BASE_URL: string;
-        WEBHOOK_SECRET: string;
-        RESEND_API_KEY: string
-    }
+  interface ProcessEnv {
+    STRIPE_SECRET_KEY: string;
+    BASE_URL: string;
+    NEXT_PUBLIC_BASE_URL: string;
+    WEBHOOK_SECRET: string;
+    RESEND_API_KEY: string;
+    GOOGLE_SHEET_SPREADSHEET_ID: string;
+    GOOGLE_PRIVATE_KEY_ID: string;
+    CLIENT_GOOGLE_EMAIL: string;
+    GOOGLE_PRIVATE_KEY: string;
+  }
 }
--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,64 +1,142 @@
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import { Stripe } from 'stripe';
-import { Resend } from 'resend';
+import { Stripe } from "stripe";
+import { Resend } from "resend";
 import { ShippingDetails } from "@/components/email/shippingDetails";
 import { ShippingAdminDetails } from "@/components/email/shippingAdmin";
-
+import { google } from "googleapis";
 
 export async function POST(request: NextRequest) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+  const body = await request.text();
+  const headersList = headers();
+  const sig: string = headersList.get("stripe-signature") || "";
 
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-    const body = await request.text()
-    const headersList = headers()
-    const sig: string = headersList.get('stripe-signature') || ''
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-    const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(body, sig, WEBHOOK_SECRET);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: error }, { status: 400 });
+  }
+  switch (event.type) {
+    case "checkout.session.completed":
+      //TODO change default fields with better options such custom fields with customername
+      const checkoutSessionCompleted = event.data.object;
+      const customerAddress: any =
+        checkoutSessionCompleted.customer_details?.address || "not found";
+      const customerEmail =
+        checkoutSessionCompleted.customer_email ||
+        checkoutSessionCompleted.customer_details?.email ||
+        "not found";
+      const customerName =
+        checkoutSessionCompleted.customer_details?.name || "not found";
+      const resend = new Resend(process.env.RESEND_API_KEY);
 
-    let event;
-    try {
-        event = stripe.webhooks.constructEvent(body, sig, WEBHOOK_SECRET)
-    } catch (error) {
-        console.error(error)
-        return NextResponse.json({ error: error }, { status: 400 })
-    }
-    switch (event.type) {
-        case 'checkout.session.completed':
+      saveDataToGoogleSheets(
+        new Date(event.created * 1000),
+        customerName,
+        customerName,
+        customerAddress?.line1,
+        customerAddress?.line2,
+        customerAddress.postal_code,
+        customerAddress.state,
+        customerEmail,
+        event.id
+      );
 
-            //TODO change default fields with better options such custom fields with customername 
-            const checkoutSessionCompleted = event.data.object
-            const customerAddress = checkoutSessionCompleted.customer_details?.address || 'not found'
-            const customerEmail = checkoutSessionCompleted.customer_email || checkoutSessionCompleted.customer_details?.email || 'not found'
-            const customerName = checkoutSessionCompleted.customer_details?.name || 'not found'
-            const resend = new Resend(process.env.RESEND_API_KEY);
+      try {
+        await resend.emails.send({
+          from: "compras@opogacela.es",
+          to: [customerEmail],
+          subject: "Compra realizada con éxito",
+          react: ShippingDetails({
+            name: customerName,
+            email: customerEmail,
+            address: customerAddress,
+          }) as React.ReactElement,
+        });
 
-            try {
-                await resend.emails.send({
-                    from: 'compras@opogacela.es',
-                    to: [customerEmail],
-                    subject: "Compra realizada con éxito",
-                    react: ShippingDetails({ name: customerName, email: customerEmail, address: customerAddress }) as React.ReactElement,
-                });
+        await resend.emails.send({
+          from: "compras@opogacela.es",
+          to: [
+            // "pilar.soldado@gmail.com",
+            "borjamrd1@gmail.com",
+          ],
+          subject: "Nueva compra en OPOGACELA",
+          react: ShippingAdminDetails({
+            name: customerName,
+            email: customerEmail,
+            address: customerAddress,
+          }) as React.ReactElement,
+        });
+      } catch (error) {
+        return Response.json({ error });
+      }
 
-                await resend.emails.send({
-                    from: 'compras@opogacela.es',
-                    to: ['pilar.soldado@gmail.com', 'borjamrd1@gmail.com'],
-                    subject: "Nueva compra en OPOGACELA",
-                    react: ShippingAdminDetails({ name: customerName, email: customerEmail, address: customerAddress }) as React.ReactElement,
-                });
-            } catch (error) {
-                return Response.json({ error });
-            }
+      break;
 
-            break;
+    default:
+      console.log(`Evento no manejado: ${event.type}`);
+      break;
+  }
 
-        default:
-            console.log(`Evento no manejado: ${event.type}`)
-            break;
-    }
+  async function saveDataToGoogleSheets(
+    date: any,
+    name: string,
+    surname: string,
+    line1: Stripe.Address | undefined | null,
+    line2: Stripe.Address | undefined | null,
+    cp: any,
+    state: any,
+    email: string,
+    idProduct: any
+  ) {
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        type: "service_account",
+        project_id: "opogacela",
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        client_email: process.env.CLIENT_GOOGLE_EMAIL,
+        client_id: "115968510702393221802",
 
+        universe_domain: "googleapis.com",
+      },
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
 
+    const client: any = await auth.getClient();
 
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+    const spreadsheetId = process.env.GOOGLE_SHEET_SPREADSHEET_ID;
 
-    return NextResponse.json({ event }, { status: 200 })
+    const getRows = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "A:L",
+    });
+
+    const numRows = getRows.data.values ? getRows.data.values.length : 0;
+
+    // Calcular el rango dinámico para la nueva fila
+    const range = `A${numRows + 1}:L${numRows + 1}`;
+
+    // Añadir una nueva fila con los datos proporcionados
+    await googleSheets.spreadsheets.values.append({
+      auth,
+      spreadsheetId,
+      range,
+      valueInputOption: "USER_ENTERED",
+      requestBody: {
+        values: [
+          [date, name, surname, line1, line2, cp, state, email, idProduct],
+        ],
+      },
+    });
+  }
+
+  return NextResponse.json({ event }, { status: 200 });
 }
