83b72ec22ce686f633c9ead111f9abbcbf4065be
Borja <borjam@oseainnovations.com>
2024-05-07 21:37:43 +0200

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,19 @@
-import "./globals.css"
-import { Inter as FontSans } from "next/font/google"
+import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { cn } from "@/lib/utils";
 import { Metadata } from "next";
-import Favicon from '/public/favicon.ico';
-
+import Favicon from "/public/favicon.ico";
+import { Analytics } from "@vercel/analytics/react";
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
-
+});
 
 export const metadata: Metadata = {
   title: "Opogacela",
   description: "esquemas y recursos para opositores",
-  icons: [{ rel: 'icon', url: Favicon.src }],
-
+  icons: [{ rel: "icon", url: Favicon.src }],
 };
 
 export default function RootLayout({
@@ -31,12 +29,15 @@ export default function RootLayout({
           "lg:min-h-screen h-full bg-background font-sans antialiased",
           fontSans.variable
         )}
-      > <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
       >
+        {" "}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Analytics />
           {children}
         </ThemeProvider>
       </body>
