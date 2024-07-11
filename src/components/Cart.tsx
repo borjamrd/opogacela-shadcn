6e0c3625f152730f50ea1da335711f19a4405c23
Borja <borjam@oseainnovations.com>
2024-07-11 19:31:52 +0200

    hide paymen options

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -94,7 +94,7 @@ export default function Cart() {
         <AlertDialogFooter>
           <div className="p-2 rounded-sm bg-red-200 text-red-700 font-bold text-balance">
             Provisionalmente la forma de pago será exclusivamente mediante Bizum
-            al número 648 78 72 88. En breve reactivaremos de nuevo las demás
+            al número 658 78 72 88. En breve reactivaremos de nuevo las demás
             formas de pago con tarjeta.
           </div>
           <AlertDialogCancel>Cerrar</AlertDialogCancel>
